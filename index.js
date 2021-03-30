b33cae88f5fb7d14db706377617717d14ca064c9
unknown <kbp4154@naver.com>
2021-03-30 14:53:30 +0900

    8

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://koko:abcd1234@boilerplater.hhnv0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -8,6 +17,20 @@ mongoose.connect('mongodb+srv://koko:abcd1234@boilerplater.hhnv0.mongodb.net/myF
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 
-app.get('/',(req,res) => res.send('Hello World!'))
+app.get('/',(req,res) => res.send('Hello World! ~~~~아아아아'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/register', (req, res) => {
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))       
